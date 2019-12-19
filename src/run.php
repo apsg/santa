37d38f9c689cfdd9880b201de7285e9d047002a7
Szymon Gackowski <szymon.gackowski@gmail.com>
2019-12-19 10:49:54 +0100

    added try-catch

--- src/run.php
+++ src/run.php
@@ -6,6 +6,16 @@ $part = $argv[2];
 
 $classname = "\\Apsg\\Santa\\Days\\Day{$day}";
 
-call_user_func($classname . '::part' . $part);
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, $severity, $severity, $file, $line);
+});
+
+try {
+    call_user_func($classname . '::part' . $part);
+} catch (ErrorException $e) {
+    echo 'Wrong input';
+}
+
+restore_error_handler();
 
 echo PHP_EOL;
