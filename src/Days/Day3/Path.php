71f0856ee9bdaf944bc57f40a861ae8deb1f8fc2
Szymon Gackowski <szymon.gackowski@gmail.com>
2019-12-19 11:42:41 +0100

    Day 3-1

---
+++ src/Days/Day3/Path.php
@@ -0,0 +1,102 @@
+<?php
+namespace Apsg\Santa\Days\Day3;
+
+class Path
+{
+    /** @var array */
+    protected $path = [];
+
+    public function __construct(string $path = '')
+    {
+        $this->path = [];
+
+        $this->parse($path);
+    }
+
+    public function add(Point $point)
+    {
+        $this->path[$point->key()] = $point;
+    }
+
+    public function move(string $move) : self
+    {
+        $direction = substr($move, 0, 1);
+        $length = substr($move, 1);
+
+        $x = 0;
+        $y = 0;
+
+        if ($direction == 'L') {
+            $this->moveHorizontally($length, -1);
+        }
+        if ($direction == 'R') {
+            $this->moveHorizontally($length, 1);
+        }
+        if ($direction == 'D') {
+            $this->moveVertically($length, -1);
+        }
+        if ($direction == 'U') {
+            $this->moveVertically($length, 1);
+        }
+
+        return $this;
+    }
+
+    protected function moveHorizontally(int $distance, int $sign = 1) : self
+    {
+        $last = $this->last();
+
+        for ($i = 1; $i <= $distance; $i++) {
+            $this->add(new Point($last->x + $sign * $i, $last->y));
+        }
+
+        return $this;
+    }
+
+    protected function moveVertically(int $distance, int $sign = 1) : self
+    {
+        $last = $this->last();
+
+        for ($i = 1; $i <= $distance; $i++) {
+            $this->add(new Point($last->x, $last->y + $sign * $i));
+        }
+
+        return $this;
+    }
+
+    public function parse(string $path) : self
+    {
+        foreach (explode(',', $path) as $move) {
+            $this->move($move);
+        }
+
+        return $this;
+    }
+
+    public function intersections(self $otherPath) : array
+    {
+        $intersections = [];
+
+        foreach ($this->path as $key => $value) {
+            if ($otherPath->exists($key)) {
+                $intersections[] = $value;
+            }
+        }
+
+        return $intersections;
+    }
+
+    public function exists(string $key) : bool
+    {
+        return isset($this->path[$key]);
+    }
+
+    protected function last() : Point
+    {
+        if (empty($this->path)) {
+            return new Point(0, 0);
+        }
+
+        return end($this->path);
+    }
+}
---
+++ src/Days/Day3/Point.php
@@ -0,0 +1,27 @@
+<?php
+namespace Apsg\Santa\Days\Day3;
+
+class Point
+{
+    /** @var int */
+    public $x;
+
+    /** @var int */
+    public $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function equals(self $point)
+    {
+        return $this->x == $point->x && $this->y == $point->y;
+    }
+
+    public function key() : string
+    {
+        return "{$this->x}:{$this->y}";
+    }
+}
